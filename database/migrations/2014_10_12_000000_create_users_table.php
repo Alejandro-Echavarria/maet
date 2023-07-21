4ec91186075cca1021e9ae0655c9dfb7c17d8863
Manuel Echavarria <mechavarria@aldereca.com>
2023-07-21 17:10:22 -0400

    [Avances] Se crearon las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,14 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('position', 45)->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->text('bio')->nullable();
+            $table->date('birthday')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('resumen_path', 2048)->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_07_21_200754_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+
+            $table->id();
+            $table->foreignId('category_id');
+            $table->foreignId('client_id')->nullable();
+            $table->string('title');
+            $table->string('logo_url')->nullable();
+            $table->string('color');
+            $table->string('project_name');
+            $table->string('languages');
+            $table->text('preview');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
