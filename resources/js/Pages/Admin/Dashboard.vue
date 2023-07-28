82013715925afa81861ec583521e49c04bf98785
Manuel Echavarria <mechavarria@aldereca.com>
2023-07-28 17:16:50 -0400

    [Avances] SideNav y Nav

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,7 +1,14 @@
 <script setup>
-import SideNav from '@/Components/Main/Admin/Layout/MainLayout.vue';
+import MainLayout from '@/Components/Main/Admin/Layout/MainLayout.vue';
 </script>
 
 <template>
-    <SideNav/>
+    <MainLayout class="pt-12">
+        <h3 class="my-12 text-[1.75rem] font-bold leading-[1.2]">
+            Resize to change the mode
+        </h3>
+        <div>
+            <img class="inline-block w-[80%] rounded" src="https://tecdn.b-cdn.net/img/Photos/Others/img%20(53).webp" />
+        </div>
+    </MainLayout>
 </template>
