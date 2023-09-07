f5e32dfa1689d64e3fd0df494e2d4b2bb86a1900
Manuel Echavarria <mechavarria@aldereca.com>
2023-09-07 11:20:03 -0400

    fix

--- resources/js/Components/Main/Admin/Components/Selects/VueSelect.vue
+++ resources/js/Components/Main/Admin/Components/Selects/VueSelect.vue
@@ -26,12 +26,11 @@ import 'vue-select/dist/vue-select.css';
 }
 
 .style-chooser input.vs__search,
-.input.vs__search:focus {
+input.vs__search:focus {
     border: 0px;
     padding: 0px;
     margin: 4px;
     outline: none;
-    border: none;
 }
 
 .style-chooser .vs__search::placeholder,
