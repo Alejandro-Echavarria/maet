c46a1bae5f62121df3685634294ae6957e782fe5
Manuel Echavarria <mechavarria@aldereca.com>
2023-11-29 10:57:53 -0400

    finalizacion sidebar

--- resources/js/Components/Main/Admin/Layout/Nav/NavItem.vue
+++ resources/js/Components/Main/Admin/Layout/Nav/NavItem.vue
@@ -34,25 +34,20 @@ const toggleSidebarVisibility = () => {
                 'flex whitespace-nowrap items-center py-2 px-3 text-sm font-semibold text-gray-700 rounded-lg dark:text-gray-200 hover:bg-indigo-200 dark:hover:bg-indigo-700 group transition duration-300 ease-linear',
                 { 'bg-indigo-100 text-indigo-700': $page.url.startsWith(item.activeClass) },
             ]">
-            <!-- <font-awesome-icon class="w-5 h-5" :icon="['fas', 'chart-line']" /> -->
-            <span class="flex-1 break-keep">{{ item.label }}</span>
+            <span class="flex-1">
+                <font-awesome-icon class="w-4 h-4 mr-2" :icon="item.icon" />
+                {{ item.label }}
+            </span>
             </Link>
             <Disclosure v-else v-slot="{ open }" :default-open="hasActiveChild">
                 <DisclosureButton :class="[
                     'flex w-full whitespace-nowrap text-left items-center py-2 px-3 text-sm font-semibold text-gray-700 rounded-lg dark:text-gray-200 hover:bg-indigo-200 dark:hover:bg-indigo-700 group transition duration-300 ease-linear',
                     open ? 'text-indigo-700' : '',
                 ]">
-
-                    <svg class="w-5 h-5 mr-2 shrink-0" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z">
-                        </path>
-                    </svg>
-
                     <span :class="['flex-1',
                         open ? 'text-indigo-700' : '',
                     ]">
+                        <font-awesome-icon class="w-4 h-4 mr-2" :icon="item.icon" />
                         {{ item.label }}
                     </span>
 
