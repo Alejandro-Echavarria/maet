14c49d36d802e0582f975baefaa5a7bd2e96fcd6
Manuel Echavarria <mechavarria@aldereca.com>
2023-11-30 16:23:06 -0400

    [release] mejoras sidebar
    1. Se mejoró la transición del Disclosure Panel en el sidebar.
    2. Se agregó un border a los Disclosure Panel.

--- resources/js/Components/Main/Admin/Layout/MainLayout.vue
+++ resources/js/Components/Main/Admin/Layout/MainLayout.vue
@@ -1,13 +1,11 @@
 <script setup>
 // import SecondNav from "@/Components/Main/Admin/Layout/Nav/SecondNav.vue";
-import Sidebar2 from "@/Components/Main/Admin/Layout/Nav/Sidebar2.vue";
+import Navbar from "@/Components/Main/Admin/Layout/Nav/Navbar.vue";
 </script>
 
 <template>
     <div>
-        <Sidebar2 />
-
-        <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/5025H8K1wbI?si=jc-T7efm5o_wxYc6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+        <Navbar />
 
         <main class="xs:px-0 lg:p-6 mx-4 lg:ml-64">
             <div class="2xl:max-w-[1500px] max-w-full mx-auto">
