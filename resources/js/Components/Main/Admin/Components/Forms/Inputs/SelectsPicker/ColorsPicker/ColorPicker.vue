33c9c7d7f178d01f298db20735360f9b4e8a856f
Manuel Echavarria <mechavarria@aldereca.com>
2023-12-01 17:02:54 -0400

    Color component done

--- resources/js/Components/Main/Admin/Components/Forms/Inputs/SelectsPicker/ColorsPicker/ColorPicker.vue
+++ resources/js/Components/Main/Admin/Components/Forms/Inputs/SelectsPicker/ColorsPicker/ColorPicker.vue
@@ -1,42 +1,114 @@
 <script setup>
-import { ref } from "vue";
-import TextInput from '@/Components/TextInput.vue';
+import { ref, onMounted } from "vue";
 
-const isOpen = ref(false);
-const currentColor = ref('gray-100');
-const colors = ["gray", "slate", "red", "orange", "yellow", "green", "blue", "indigo", "purple", "pink"];
-const variants = [100, 200, 300, 400, 500, 600];
+const props = defineProps({
+    modelValue: String,
+});
+
+const emit = defineEmits(['update:modelValue']);
+const open = ref(false);
+const currentColor = ref(props.modelValue);
+const colors = [
+    "gray",
+    "slate",
+    "pink",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "indigo",
+    "purple",
+];
+const variants = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const iconColor = ref('text-gray-700');
+const regex = /^([a-z]+)-(\d+)$/;
+const baseColor = ref(null);
+const shade = ref(null);
+
+onMounted(() => {
+    const result = splitColor(props.modelValue);
+
+    if (result) {
+        baseColor.value = result.base;
+        shade.value = result.shadeValue;
+
+        if (parseInt(shade.value) > 500 ) {
+            iconColor.value = 'text-gray-200';
+        }
+    }
+});
 
 const selectColor = (color, variant) => {
     currentColor.value = `${color}-${variant}`;
+
+    emit('update:modelValue', currentColor.value);
+
+    if (variant < 500) {
+        setIconBlack();
+    } else {
+        setIconWhite();
+    }
+
+    open.value = false;
 };
 
+const setIconWhite = () => {
+    iconColor.value = 'text-gray-100';
+};
 
+const setIconBlack = () => {
+    iconColor.value = 'text-gray-700';
+};
+
+const splitColor = (color) => {
+    const matches = color.match(regex);
+
+    if (matches && matches.length === 3) {
+        const [, base, shadeValue] = matches;
+        return { base, shadeValue };
+    }
+    return null; // Manejar el caso en el que la cadena no coincida con el formato esperado
+};
 </script>
 
 <template>
-    <div>
-        <div class="flex flex-row relative">
-            <TextInput />
-            <div @click="isOpen = !isOpen" class="cursor-pointer rounded-full ml-3 my-auto h-10 w-10 flex"
-                :class="`bg-${currentColor}`">
-                <svg xmlns="http://www.w3.org/2000/svg" :class="`${iconColor}`" class="h-6 w-6 mx-auto my-auto" fill="none"
+    <div class="relative">
+        <!-- Full Screen Overlay -->
+        <div v-show="open" class="fixed inset-0 z-40" @click="open = false" />
+
+        <div class="flex flex-row w-full justify-between">
+            <div class="w-full">
+                <span
+                    class="cursor-pointer block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:ring-indigo-700 focus:border-indigo-700 peer transition"
+                    @click="open = true">
+                    {{ currentColor }}
+                </span>
+            </div>
+            <div @click="open = !open" :class="['cursor-pointer rounded-full my-auto h-10 w-10 flex shrink-0', `bg-${currentColor}`]">
+                <svg xmlns="http://www.w3.org/2000/svg" :class="['h-6 w-6 mx-auto my-auto', iconColor ]" fill="none"
                     viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
                 </svg>
             </div>
         </div>
+
         <transition name="fade">
-            <div v-show="isOpen" class="z-10 absolute right-2 mt-2 rounded-xl shadow-lg max-w-xs sm:max-w-md">
-                <div class="rounded-xl bg-white shadow-xs p-2 z-10 border overflow-auto w-auto">
+            <div v-show="open"
+                class="z-50 absolute origin-top-right right-0 mt-2 rounded-xl shadow-lg max-w-xs sm:max-w-md">
+                <div class="rounded-xl bg-white shadow-xs p-2 z-10 border overflow-auto w-[17rem] sm:w-full h-32">
                     <div class="flex">
                         <template v-for="color in colors">
                             <div>
                                 <template v-for="variant in variants">
-                                    <div @click="selectColor(color, variant)"
-                                        class="cursor-pointer w-6 h-6 rounded-md mx-1 my-1"
-                                        :class="`bg-${color}-${variant}`"></div>
+                                    <div @click="selectColor(color, variant)" :class="[
+                                        'cursor-pointer w-6 h-6 rounded-md mx-1 my-1',
+                                        `bg-${color}-${variant}`,
+                                        { 'border-4 border-x-black border-y-gray-200 border-dashed': currentColor === `${color}-${variant}` }
+                                    ]">
+                                    </div>
                                 </template>
                             </div>
                         </template>
