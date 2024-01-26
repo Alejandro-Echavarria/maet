95fc36781c0c49a319fb5dc2ed45e19054434b10
Manuel Echavarria <mechavarria@aldereca.com>
2024-01-26 12:08:36 -0400

    transition modal for mobile2

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -73,7 +73,7 @@ const maxWidthClass = computed(() => {
                 </transition>
 
                 <transition enter-active-class="ease-out duration-200"
-                    enter-from-class="opacity-0 translate-y-full sm:translate-y-0 sm:scale-95"
+                    enter-from-class="opacity-0 translate-y-64 sm:translate-y-0 sm:scale-95"
                     enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
                     leave-from-class="opacity-100 translate-y-0 sm:scale-100"
                     leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
