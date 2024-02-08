97e44cd7f42b3cb7cf6181ab83e361771e416a8e
Manuel Echavarria <mechavarria@aldereca.com>
2024-02-08 14:02:08 -0400

    SEO Banner

--- resources/js/Components/Main/Banners/MainBanner.vue
+++ resources/js/Components/Main/Banners/MainBanner.vue
@@ -13,13 +13,16 @@ const { user } = defineProps({
         <div class="backdrop-blur-2xl bg-gradient-to-b from-white/80 to-blue-50/10 h-full">
             <Container class="h-full flex items-center">
                 <div class="space-y-8 w-full flex flex-col">
-                    <div class="w-full leading-relaxed">
-                        <h4 class="text-5xl md:text-7xl font-bold bg-gradient-to-r from-zinc-700 via-indigo-700 to-blue-700 bg-clip-text text-transparent">
-                            Hey,
+                    <div class="w-auto leading-relaxed">
+                        <h4
+                            class="text-5xl md:text-7xl font-bold bg-gradient-to-r from-zinc-700 via-indigo-700 to-blue-700 bg-clip-text text-transparent">
+                            <span>
+                                Hey,
+                            </span>
                         </h4>
                         <h1 class="animate-fade-in-left text-5xl md:text-7xl font-bold bg-gradient-to-r from-zinc-700 via-indigo-700 to-blue-700 bg-clip-text text-transparent"
                             style="animation-delay: 0.1s; animation-duration: 0.4s;">
-                            <span>I'm</span> {{ user.name }}
+                            <span>I'm</span> <span>{{ user.name }}</span>
                         </h1>
                     </div>
                     <div class="w-full grid justify-items-end">
@@ -37,7 +40,8 @@ const { user } = defineProps({
                 </div>
             </Container>
         </div>
-        <div class="w-32 sm:w-64 absolute top-10 right-20 z-[-1] animate-fade-in" style="fill: #6b56cb; animation-delay: 1.1s;">
+        <div class="w-32 sm:w-64 absolute top-10 right-20 z-[-1] animate-fade-in"
+            style="fill: #6b56cb; animation-delay: 1.1s;">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 747.2 726.7">
                 <path
                     d="M539.8 137.6c98.3 69 183.5 124 203 198.4 19.3 74.4-27.1 168.2-93.8 245-66.8 76.8-153.8 136.6-254.2 144.9-100.6 8.2-214.7-35.1-292.7-122.5S-18.1 384.1 7.4 259.8C33 135.6 126.3 19 228.5 2.2c102.1-16.8 213.2 66.3 311.3 135.4z">
