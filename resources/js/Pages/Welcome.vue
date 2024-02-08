29320c371c9ea800abce612e1832b50de0966a0c
Manuel Echavarria <mechavarria@aldereca.com>
2024-02-08 13:54:28 -0400

    SEO viewport

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -41,7 +41,7 @@ const user = props.user;
 <template>
     <Head>
         <title>Manuel Echavarria</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description"
             content="Descubre maet.dev, mi portafolio de desarrollo web. Transforma tus ideas en sitios web impactantes y funcionales. ¡Impulsa tu presencia en línea con diseños modernos, responsivos y optimizados para SEO! Explora servicios de diseño y desarrollo web de calidad.">
         <meta name="keywords"
@@ -64,7 +64,8 @@ const user = props.user;
                                 Personal project
                             </span>
                             <Link :href="route('jobs.show', job.id)" target="_blank" rel="noreferrer noopener nofollow">
-                            <h3 :class="['block mb-8 text-2xl font-bold text-gray-700 dark:text-gray-200 hover:text-indigo-700']">
+                            <h3
+                                :class="['block mb-8 text-2xl font-bold text-gray-700 dark:text-gray-200 hover:text-indigo-700']">
                                 {{ job.title }}
                             </h3>
                             </Link>
@@ -74,18 +75,18 @@ const user = props.user;
                             </p>
 
                             <div>
-                                <Link :href="route('jobs.show', job.slug)" target="_blank" rel="noreferrer noopener nofollow"
+                                <Link :href="route('jobs.show', job.slug)" target="_blank"
+                                    rel="noreferrer noopener nofollow"
                                     class="sm:w-auto w-full inline-block mt-2 text-blue-500 underline hover:text-blue-400">
-                                    <PrimaryButton class="">
-                                        Read more
-                                    </PrimaryButton>
+                                <PrimaryButton class="">
+                                    Read more
+                                </PrimaryButton>
                                 </Link>
                             </div>
                         </div>
                         <div class="w-full flex justify-end order-1 sm:order-2">
                             <img class="bg-auto sm:w-[20rem] md:w-[32rem] lg:w-[24rem] xl:w-[30rem] bg-no-repeat bg-center w-full rounded-2xl"
-                                :src="`/storage/${job?.images[0]?.url}`"
-                                :alt="job.alt_banner_image">
+                                :src="`/storage/${job?.images[0]?.url}`" :alt="job.alt_banner_image">
                         </div>
                     </div>
                 </div>
