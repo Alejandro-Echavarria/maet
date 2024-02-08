04c2ba9a48a35898216be7c6af9fa0ac3ea09777
Manuel Echavarria <mechavarria@aldereca.com>
2024-02-08 13:31:01 -0400

    SEO viewport

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="author" content="Manuel Echavarria">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
