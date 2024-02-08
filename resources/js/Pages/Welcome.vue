80590837c9a711384bf2e11ec8a84ac93ad362e8
Manuel Echavarria <mechavarria@aldereca.com>
2024-02-08 17:21:33 -0400

    LCP performance

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -84,7 +84,7 @@ const user = props.user;
                             </div>
                         </div>
                         <div class="w-full flex justify-end order-1 sm:order-2">
-                            <img fetchpriority="high" loading="lazy" class="h-96 w-full overflow-auto object-cover rounded-xl"
+                            <img fetchpriority="high" class="h-96 w-full overflow-auto object-cover rounded-xl"
                                 :src="`/storage/${job?.images[0]?.url}`" :alt="job.alt_banner_image">
                         </div>
                     </div>
