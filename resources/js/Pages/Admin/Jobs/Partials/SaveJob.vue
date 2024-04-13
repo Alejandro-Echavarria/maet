56dde317ebe98dbb0c3157eee9919661bfcb0dcf
Manuel Echavarria <mechavarria@aldereca.com>
2024-04-13 10:04:01 -0400

    Componente alerta errores en modal

--- resources/js/Pages/Admin/Jobs/Partials/SaveJob.vue
+++ resources/js/Pages/Admin/Jobs/Partials/SaveJob.vue
@@ -15,6 +15,7 @@ import Images from "@/Components/Main/Admin/Components/Forms/Inputs/Images/Image
 import ToggleSwitch from "@/Components/Main/Admin/Components/Forms/Inputs/ToggleSwitch/ToggleSwitch.vue";
 import Ckeditor from "@/Components/Main/Admin/Components/Forms/Inputs/ckeditor/Ckeditor.vue";
 import CKeditorHelper from "@/Helpers/CKeditor/Ckeditor";
+import ModalErrorAlert from "@/Components/Main/Admin/Components/OtherComponents/ModalErrorAlert.vue";
 
 const props = defineProps({
     data: Object,
@@ -263,18 +264,7 @@ defineExpose({ openModal });
 
             <template #footer>
                 <div class="flex justify-end w-full">
-                    <div class="shrink-0 mr-3" :class="Object.keys(form.errors).length > 0 ? 'flex' : 'hidden'"
-                        :title="'Existen ' + Object.keys(form.errors).length + ' errores'">
-                        <div class="bg-gray-200 h-full p-2 rounded-full flex items-center">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-red-500 animate-pulse"
-                                    viewBox="0 0 24 24">
-                                    <path fill="currentColor"
-                                        d="M12 1.67c.955 0 1.845.467 2.39 1.247l.105.16l8.114 13.548a2.914 2.914 0 0 1-2.307 4.363l-.195.008H3.882a2.914 2.914 0 0 1-2.582-4.2l.099-.185l8.11-13.538A2.914 2.914 0 0 1 12 1.67M12.01 15l-.127.007a1 1 0 0 0 0 1.986L12 17l.127-.007a1 1 0 0 0 0-1.986zM12 8a1 1 0 0 0-.993.883L11 9v4l.007.117a1 1 0 0 0 1.986 0L13 13V9l-.007-.117A1 1 0 0 0 12 8" />
-                                </svg>
-                            </span>
-                        </div>
-                    </div>
+                    <ModalErrorAlert :errors="form.errors" />
 
                     <SecondaryButton @click="closeModal" class="mr-3">
                         Close
