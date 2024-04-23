2c2a7b8e333e3392ec345d669c12f9e83c8c0be3
Manuel Echavarria <mechavarria@aldereca.com>
2024-04-23 16:59:23 -0400

    Eliminacion de paquetes no usados en el modelo

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -3,10 +3,8 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Technology extends Model
 {
