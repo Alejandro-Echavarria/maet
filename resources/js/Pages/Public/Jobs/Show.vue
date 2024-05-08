5c9a97430878e0f4a3cb6bb191e097f0a945fd2c
Manuel Echavarria <mechavarria@aldereca.com>
2024-05-08 11:48:28 -0400

    avances

--- resources/js/Pages/Public/Jobs/Show.vue
+++ resources/js/Pages/Public/Jobs/Show.vue
@@ -29,30 +29,31 @@ onMounted(() => {
 
         <div class="space-y-6 max-w-5xl mx-auto">
             <Link :href="route('jobs.index', { 'category': job.category.slug })" class="animate-fade-in-left"
-                style="animation-delay: 0.36s;">
-            <span class="py-1 px-2 text-sm text-indigo-700 font-medium border-2 border-indigo-700 rounded-full hover:bg-indigo-700/5 transition ease-in-out duration-300">
+                style="animation-delay: 1.1s;">
+            <span
+                class="py-1 px-2 text-sm text-indigo-700 font-medium border-2 border-indigo-700 rounded-full hover:bg-indigo-700/5 transition ease-in-out duration-300">
                 {{ job.category.name }}
             </span>
             </Link>
 
-            <h1 :class="`text-center text-5xl lg:text-7xl py-1.5 md:text-6xl font-bold bg-gradient-to-r from-${job.color} via-gray-800/90 to-gray-800 bg-clip-text text-transparent animate-fade-in-left`"
-                style="animation-delay: 0.1s;">
+            <h1 :class="`text-center text-5xl lg:text-7xl py-1.5 md:text-6xl font-bold bg-gradient-to-r from-${job.color} via-gray-800/90 to-gray-800 bg-clip-text text-transparent animate-fade-in-up`"
+                style="animation-delay: 0.36s;">
                 {{ job.title }}
             </h1>
 
             <div class="space-y-4">
-                <div class="flex justify-center flex-wrap items-center gap-2">
+                <div class="flex justify-center flex-wrap items-center gap-2 animate-fade-in-up" style="animation-delay: 0.72s;">
                     <div v-if="job.link" class="flex flex-wrap items-center text-gray-600">
                         <a :href="job.link" target="_blank" rel="noreferrer noopener nofollow"
-                            class="flex px-2  animate-fade-in-left" style="animation-delay: 0.46s;"
-                            :title="job.title">
-                            <span class="text-sm py-1 px-2 text-indigo-700 border-2 border-indigo-700 font-medium rounded-full hover:bg-indigo-700/5 transition ease-in-out duration-300">
+                            class="flex px-2" :title="job.title">
+                            <span
+                                class="text-sm py-1 px-2 text-indigo-700 border-2 border-indigo-700 font-medium rounded-full hover:bg-indigo-700/5 transition ease-in-out duration-300">
                                 Visit the project
                             </span>
                         </a>
                     </div>
 
-                    <time class="animate-fade-in-left" style="animation-delay: 0.56s;">
+                    <time>
                         <span class="font-medium text-sm text-gray-600">
                             {{ job.created_at }}
                         </span>
@@ -60,10 +61,10 @@ onMounted(() => {
                 </div>
             </div>
 
-            <div class="flex justify-center flex-wrap gap-4 items-center text-gray-600">
+            <div class="flex justify-center flex-wrap gap-4 items-center text-gray-600 animate-fade-in" style="animation-delay: 1.1s;">
                 <div v-for="(technology, index) in job.technologies" :key="'technology-' + technology.id">
                     <Link :href="route('jobs.index', { 'technology': technology.slug })">
-                    <div id="tech-container" class="flex gap-1 text-sm animate-fade-in-left"
+                    <div id="tech-container" class="flex gap-1 text-sm animate-fade-in-up"
                         :style="{ animationDelay: `${index * 0.11}s` }">
                         <span v-html="technology.icon" :title="technology.name"></span>
                         <!-- <p>
@@ -74,12 +75,12 @@ onMounted(() => {
                 </div>
             </div>
 
-            <div class="text-center content-ckeditor mb-4 animate-fade-in" style="animation-delay: 0.76s;">
+            <div class="text-center content-ckeditor mb-4 animate-fade-in-up" style="animation-delay: 1.1s;">
                 <p v-html="job.preview" />
             </div>
 
             <div class="rounded-xl overflow-hidden shadow-gray-500/20 shadow-lg sm:max-h-[40rem] animate-fade-in-up"
-                style="animation-delay: 0.86s;">
+                style="animation-delay: 1.1s;">
                 <div>
                     <div class="flex h-6 w-full items-center gap-5 bg-gray-800 px-3">
                         <div class="flex items-center gap-1">
