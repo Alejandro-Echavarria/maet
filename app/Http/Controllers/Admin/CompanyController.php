13ed81aeb3849aa6c6b43d036330d4e50cb3a8b0
Manuel Alejandro Echavarría Taveras <manuelalejandroechavarriataveras@Manuels-MacBook-Air.local>
2024-05-09 22:26:12 -0400

    avances

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Company;
 use App\Models\CompanyType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class CompanyController extends Controller
@@ -16,5 +17,42 @@ class CompanyController extends Controller
         $companyTypes = CompanyType::all();
 
         return Inertia::render('Admin/Companies/Index', compact('companies', 'companyTypes'));
+    }
+
+    public function store(Request $request)
+    {
+        $request['slug'] = Str::slug($request->name);
+        $data = $request->validate(
+            [
+                'company_type_id' => 'required|exists:company_types,id',
+                'slug'            => 'required|unique:companies',
+                'name'            => 'required|max:255|string',
+                'bio'             => 'required|string',
+                'taxt_id_number'  => 'required|max:255|string',
+                'country'         => 'required|max:255|string',
+                'city'            => 'required|max:255|string',
+                'state'           => 'required|max:255|string',
+                'street'          => 'required|max:255|string',
+                'zip_code'        => 'required|max:50|string',
+                'email'           => 'required|email|max:255',
+                'phone'           => 'required|max:15',
+            ],
+            [],
+            [
+                'company_type_id' => 'company type',
+                'taxt_id_number'   => 'tax id number',
+                'zip_code'        => 'zip code',
+            ]
+        );
+
+        Company::create($data);
+
+        $page = $request?->page;
+        $search = $request?->search;
+
+        return to_route('admin.companies.index', [
+            'search' => $search,
+            'page' => $page
+        ])->with('flash', 'Company Created');
     }
 }
--- resources/js/Pages/Admin/Companies/Partials/SaveCompany.vue
+++ resources/js/Pages/Admin/Companies/Partials/SaveCompany.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useForm } from '@inertiajs/vue3';
 import DialogModal from '@/Components/DialogModal.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -10,6 +10,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import SaveAlert from '@/Helpers/Alerts/SaveAlert';
 import SimpleForm from '@/Components/Main/Admin/Components/Forms/SimpleForm.vue';
 import VueSelect from "@/Components/Main/Admin/Components/Selects/VueSelect.vue";
+import Ckeditor from "@/Components/Main/Admin/Components/Forms/Inputs/ckeditor/Ckeditor.vue";
+import CKeditorHelper from "@/Helpers/CKeditor/Ckeditor";
 
 const props = defineProps({
     data: Object,
@@ -17,19 +19,68 @@ const props = defineProps({
     page: String
 });
 
+onMounted(() => {
+    CKeditorHelper();
+});
+
 const nameInput = ref(null);
 const title = ref('');
 const modal = ref(false);
-const category = ref(null);
 const opration = ref(1);
+const company = ref(null);
 const companyTypesOptions = ref(props.data.companyTypes);
 
 const form = useForm({
+    company_type_id: null,
+    slug: '',
     name: '',
+    bio: '',
+    taxt_id_number: null,
+    country: '',
+    city: '',
+    state: '',
+    street: '',
+    zip_code: '',
+    email: '',
+    phone: '',
 });
 
+const save = () => {
+    if (opration.value === 1) {
+        form.transform((data) => ({
+            ...data,
+            search: props.filter,
+            page: props.page
+        })).post(route("admin.companies.store"), {
+            preserveScroll: true,
+            preserveState: true,
+            onSuccess: () => {
+                ok("Company created");
+            },
+            onError: () => {
+                console.log("error");
+            }
+        });
+    } else {
+        form.transform((data) => ({
+            ...data,
+            _method: 'put',
+            search: props.filter,
+            page: props.page,
+        })).post(route("admin.companies.update", job.value), {
+            preserveScroll: true,
+            preserveState: true,
+            onSuccess: () => {
+                ok("Company updated");
+            },
+            onError: () => {
+                console.log("error");
+            }
+        });
+    }
+};
 
-const openModal = (op, slug, name) => {
+const openModal = (op, data) => {
     modal.value = true;
     opration.value = op;
 
@@ -37,8 +88,19 @@ const openModal = (op, slug, name) => {
         title.value = 'Create a new company';
     } else {
         title.value = 'Edit company';
-        category.value = slug;
-        form.name = name;
+        company.value = data.slug;
+        form.company_type_id = data.company_type_id;
+        form.slug = data.slug;
+        form.name = data.name;
+        form.bio = data.bio;
+        form.taxt_id_number = data.taxt_id_number;
+        form.country = data.country;
+        form.city = data.city;
+        form.state = data.state;
+        form.street = data.street;
+        form.zip_code = data.zip_code;
+        form.email = data.email;
+        form.phone = data.phone;
     }
 
     setTimeout(() => nameInput.value.focus(), 250);
@@ -110,7 +172,7 @@ defineExpose({ openModal });
                                 
                                     :select-on-tab="true" />
 
-                                <InputError :message="form.errors.category_id" class="mt-2" />
+                                <InputError :message="form.errors.company_type_id" class="mt-2" />
                             </div>
 
                             <div class="md:col-span-3">
@@ -128,17 +190,35 @@ defineExpose({ openModal });
                             </div>
 
                             <div class="md:col-span-2">
+                                <InputLabel for="city" value="City" />
+                                <TextInput v-model="form.city" id="city" ref="cityInput" type="text" />
+
+                                <InputError :message="form.errors.city" class="mt-2" />
+                            </div>
+
+                            <div class="md:col-span-2">
                                 <InputLabel for="state" value="State" />
                                 <TextInput v-model="form.state" id="state" ref="stateInput" type="text" />
 
                                 <InputError :message="form.errors.state" class="mt-2" />
                             </div>
 
-                            <div class="md:col-span-5">
+                            <div class="md:col-span-3">
                                 <InputLabel for="street" value="Street" />
                                 <TextInput v-model="form.street" id="street" ref="streetInput" type="text" />
 
                                 <InputError :message="form.errors.street" class="mt-2" />
+                            </div>
+
+                            <div class="sm:col-span-9">
+                                <InputLabel for="body" value="Body" class="mb-2" />
+                                <Ckeditor id="body" idname="body" v-model="form.bio" :idData="company"
+                                    :additionalPath="'/company'" :urlName="urlCkeditorStoreImage" :value="form.bio"
+                                    key="bio" ref="bioInput">
+                                    <div id="ckeditorbody"></div>
+                                </Ckeditor>
+
+                                <InputError :message="form.errors.bio" class="mt-2" />
                             </div>
                         </div>
                     </template>
