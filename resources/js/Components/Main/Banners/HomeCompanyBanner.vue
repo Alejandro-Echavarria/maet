4b55de689e4d7c135f16008ed229bd99aaf14486
Manuel Alejandro Echavarría Taveras <manuelalejandroechavarriataveras@Manuels-MacBook-Air.local>
2024-06-02 19:56:45 -0400

    avances

--- resources/js/Components/Main/Banners/HomeCompanyBanner.vue
+++ resources/js/Components/Main/Banners/HomeCompanyBanner.vue
@@ -27,51 +27,37 @@ const openModal = () => {
 </script>
 
 <template>
-    <div class="w-full h-[34rem] sm:h-[83vh] relative">
-        <div
-            class="backdrop-blur-2xl bg-gradient-to-b from-white/80 to-blue-50/10 h-full"
-        >
+    <div class="w-full h-[80vh] sm:h-[80vh] relative">
+        <div class="backdrop-blur-2xl bg-gradient-to-b from-white/80 to-blue-50/10 h-full">
             <Container class="h-full flex items-center mt-12">
-                <div class="grid grid-cols-1 sm:grid-cols-2 content-center space-y-8">
+                <div class="grid grid-cols-1 sm:grid-cols-2 content-center gap-8">
                     <div class="space-y-8 grid content-center">
-                        <div
-                            class="w-full flex flex-col text-center sm:text-left"
-                        >
-                            <h1
-                                class="text-3xl lg:text-5xl font-semibold bg-gradient-to-r from-gray-800 via-indigo-700 to-blue-700 bg-clip-text text-transparent animate-fade-in-left"
+                        <div class="w-full flex flex-col text-center sm:text-left">
+                            <h1 class="text-3xl lg:text-5xl font-semibold bg-gradient-to-r from-gray-800 via-indigo-700 to-blue-700 bg-clip-text text-transparent animate-fade-in-left"
                                 style="
                                     animation-delay: 0.1s;
                                     animation-duration: 0.4s;
-                                "
-                            >
+                                ">
                                 Creating Success with Custom Web Apps
                             </h1>
 
-                            <h2
-                                class="mt-8 text-lg lg:text-xl font-semibold dark:text-white"
-                            >
+                            <h2 class="mt-8 text-lg lg:text-xl font-semibold dark:text-white">
                                 {{ user.position }}
                             </h2>
                         </div>
 
                         <div class="space-y-4">
                             <div class="flex self-end gap-4">
-                                <ContactButton
-                                    @click="openModal()"
-                                    class="rounded-lg relative shadow-md shadow-gray-500/20"
-                                >
+                                <ContactButton @click="openModal()"
+                                    class="rounded-lg relative shadow-md shadow-gray-500/20">
                                     <span class="relative flex h-2 w-2 mr-2">
                                         <span
-                                            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-200 opacity-75"
-                                        ></span>
-                                        <span
-                                            class="relative inline-flex rounded-full h-2 w-2 bg-indigo-500"
-                                        ></span>
+                                            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-200 opacity-75"></span>
+                                        <span class="relative inline-flex rounded-full h-2 w-2 bg-indigo-500"></span>
                                     </span>
                                     Get in touch
                                     <span
-                                        class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-200/0 via-indigo-200/90 to-indigo-200/0 transition-opacity duration-400 group-hover:opacity-40"
-                                    >
+                                        class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-200/0 via-indigo-200/90 to-indigo-200/0 transition-opacity duration-400 group-hover:opacity-40">
                                     </span>
                                 </ContactButton>
 
@@ -83,20 +69,16 @@ const openModal = () => {
                     </div>
 
                     <div>
-                        <img
-                            src="https://i.postimg.cc/kgd4WhyS/container.png"
-                            alt=""
-                            class="w-full h-full object-cover rounded-xl shadow-gray-500/20 shadow-lg border border-[#000000]/[0.16]"
-                        />
+                        <img src="https://i.postimg.cc/kgd4WhyS/container.png" alt=""
+                            class="w-full h-full aspect-video object-cover rounded-xl shadow-gray-500/20 shadow-lg border border-[#000000]/[0.16]" />
                     </div>
                 </div>
 
                 <div
-                    class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"
-                >
+                    class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
                     <div
-                        class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-indigo-700 opacity-20 blur-[100px]"
-                    ></div>
+                        class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-indigo-700 opacity-20 blur-[100px]">
+                    </div>
                 </div>
             </Container>
         </div>
--- resources/js/Pages/Public/Home/HomeCompany.vue
+++ resources/js/Pages/Public/Home/HomeCompany.vue
@@ -28,26 +28,19 @@ const openModal = () => {
 </script>
 
 <template>
+
     <Head>
         <title>Home</title>
-        <meta
-            type="description"
-            name="description"
-            head-key="description"
-            content="Transforming ideas into functional webapps. Boosting online presence with modern designs optimized for SEO! Software Engineering & Web Development"
-        />
+        <meta type="description" name="description" head-key="description"
+            content="Transforming ideas into functional webapps. Boosting online presence with modern designs optimized for SEO! Software Engineering & Web Development" />
     </Head>
 
     <Navbar :entity="entity" :animation="true">
         <template #button>
-            <ContactButton
-                @click="openModal()"
-                class="rounded-2xl relative shadow-md shadow-gray-500/20"
-            >
+            <ContactButton @click="openModal()" class="rounded-2xl relative shadow-md shadow-gray-500/20">
                 Contact
                 <span
-                    class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-200/0 via-indigo-200/90 to-indigo-200/0 transition-opacity duration-400 group-hover:opacity-40"
-                >
+                    class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-200/0 via-indigo-200/90 to-indigo-200/0 transition-opacity duration-400 group-hover:opacity-40">
                 </span>
             </ContactButton>
         </template>
@@ -56,59 +49,38 @@ const openModal = () => {
     <HomeCompanyBanner :entity="entity" :user="user" />
 
     <main class="xs:px-0 mx-4">
-        <div
-            class="grid justify-center h-[10vh] mt-2 gap-4 sm:gap-4m animate-fade-in-up"
-            style="animation-delay: 1.1s"
-        >
+        <div class="grid justify-center h-[10vh] mt-2 gap-4 sm:gap-4m animate-fade-in-up" style="animation-delay: 1.1s">
             <div class="flex justify-center">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                >
-                    <path
-                        d="M12 20L12 4"
-                        class="stroke-indigo-700"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                    <path
-                        d="M17 15C17 15 13.3176 20 12 20C10.6824 20 7 15 7 15"
-                        class="stroke-indigo-700"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
+                    <path d="M12 20L12 4" class="stroke-indigo-700" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M17 15C17 15 13.3176 20 12 20C10.6824 20 7 15 7 15" class="stroke-indigo-700"
+                        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
             </div>
         </div>
 
         <div class="max-w-5xl mx-auto">
-                <div class="space-y-10 sm:space-y-20">
-                    <div class="relative pb-1" v-if="jobs.length > 0">
-                        <section id="projects">
-                            <SectionTitle>
-                                <h2 class="py-4 text-5xl lg:text-7xl font-bold text-gray-800 dark:text-gray-200">
-                                    Projects
-                                </h2>
-                            </SectionTitle>
+            <div class="space-y-10 sm:space-y-20">
+                <div class="relative pb-1" v-if="jobs.length > 0">
+                    <section id="projects">
+                        <SectionTitle>
+                            <h2 class="py-4 text-5xl lg:text-7xl font-bold text-gray-800 dark:text-gray-200">
+                                Projects
+                            </h2>
+                        </SectionTitle>
 
-                            <BetweenView :projects="jobs" />
-                        </section>
+                        <BetweenView :projects="jobs" />
+                    </section>
 
-                        <div class="flex justify-center">
-                            <Link :href="route('jobs.index')">
-                            <PrimaryButton>
-                                View all
-                            </PrimaryButton>
-                            </Link>
-                        </div>
+                    <div class="flex justify-center">
+                        <Link :href="route('jobs.index')">
+                        <PrimaryButton> View all </PrimaryButton>
+                        </Link>
                     </div>
                 </div>
             </div>
+        </div>
     </main>
 
     <MainFooter />
